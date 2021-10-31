af46c35653b1c7fb637e72c10e75d172b78af253
AButsai <butsai@ukr.net>
2021-11-01 00:26:24 +0200

    first commit

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  console.log(toggleModal);
+})();
+
+// const openBtn = document.querySelector('.button');
+// const closeBtn = document.querySelector('.close-btn');
+// const modal = document.querySelector('.backdrop');
+
+// const open = openBtn.addEventListener('click', toggle);
+
+// const close = closeBtn.addEventListener('click', toggle);
+
+// function toggle(e) {
+//   modal.classList.toggle('is-hidden');
+// }
